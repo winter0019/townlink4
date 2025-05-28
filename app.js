e49bd413b6e17a03d8144af2e71b4acf18838d2a
winter0019 <121707096+winter0019@users.noreply.github.com>
2025-05-28 14:48:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const API_BASE_URL = 'http://localhost:3000/api';
+  const API_BASE_URL = 'https://your-townlink-api.onrender.com/api/businesses'; // Use the URL you got from Render for your backend
 
   const businessesContainer = document.getElementById('business-listings');
   const searchInput = document.getElementById('searchInput');
